76ffdf1338829dad68fe00fd98aa40a9fd059e5e
Harry Brooker <harrybrooker99@gmail.com>
2022-10-15 23:42:13 +1100

    Create react picker

--- quote/src/App.tsx
+++ quote/src/App.tsx
@@ -1,19 +1,28 @@
 import React, { ChangeEvent, useEffect, useState } from "react";
 import "./App.css";
 
+const url =
+  "https://script.google.com/macros/s/AKfycbwHLLiV2BP-y1Rf9kIKKlXZEUz0MZ8FCjcDaqmAmxwSGFHyXWMt2IZgMMo_8UmRa6CNww/exec";
+
 function App() {
   const [options, setOptions] = useState([]);
   const [option, setOption] = useState("");
-  const [loading, setLoading] = useState(true);
+  const [cost, setCost] = useState("");
+  const [loadingOptions, setLoadingOptions] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+  const [loadingCost, setLoadingCost] = useState(false);
+
   useEffect(() => {
-    fetch(
-      "https://script.google.com/macros/s/AKfycbyhSewNhd8OEL6Qkzno5bkUaLstwmxuyjiCpIT43THikZ4ScdMFhyvgZFwtKHei_EL5kw/exec?query=options"
-    )
+    setLoadingOptions(true);
+    fetch(`${url}?query=options`)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-        setOptions(data.options);
-        setLoading(false);
+        setTimeout(function () {
+          console.log(data);
+          setOptions(data.options);
+          setOption(data.options[0]);
+          setLoadingOptions(false);
+        }, 2000);
       })
       .catch((err) => {
         console.log(err.message);
@@ -26,14 +35,35 @@ function App() {
 
   function handleSubmit(event: React.SyntheticEvent<HTMLFormElement>) {
     event.preventDefault();
-    alert("A name was submitted: " + option);
+    setLoadingCost(true);
+    setSubmitted(true);
+    console.log(option);
+
+    fetch(`${url}?query=cost&option=${option}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setCost(data.cost);
+        setLoadingCost(false);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
   }
 
-  return loading ? null : (
+  return loadingOptions ? (
+    <div className="App">
+      <header className="App-header">loading...</header>
+    </div>
+  ) : loadingCost ? (
+    <div className="App">
+      <header className="App-header">calculating...</header>
+    </div>
+  ) : !submitted ? (
     <div className="App">
       <header className="App-header">
         <form onSubmit={handleSubmit}>
-          <label>How many transactions have you had this year</label>
+          <label>How many transactions have you had this year?</label>
           <br></br>
           <select value={option} onChange={onChange}>
             {options.map((x) => (
@@ -43,6 +73,10 @@ function App() {
           <input type="submit" value="Submit" />
         </form>
       </header>
+    </div>
+  ) : (
+    <div className="App">
+      <header className="App-header">Estimated cost: {cost}</header>
     </div>
   );
 }
