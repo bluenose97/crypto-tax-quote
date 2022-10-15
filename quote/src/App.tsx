2c2916f486037f0642d40cd8f19febfb8eb4b77e
Harry Brooker <harrybrooker99@gmail.com>
2022-10-15 23:09:57 +1100

    Create react picker

--- quote/src/App.tsx
+++ quote/src/App.tsx
@@ -1,23 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
+  const [options, setOptions] = useState([]);
+  const [option, setOption] = useState("");
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch(
+      "https://script.google.com/macros/s/AKfycbyhSewNhd8OEL6Qkzno5bkUaLstwmxuyjiCpIT43THikZ4ScdMFhyvgZFwtKHei_EL5kw/exec?query=options"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setOptions(data.options);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+  function onChange(event: ChangeEvent<HTMLSelectElement>) {
+    setOption(event.target.value); //update your value here
+  }
+
+  function handleSubmit(event: React.SyntheticEvent<HTMLFormElement>) {
+    event.preventDefault();
+    alert("A name was submitted: " + option);
+  }
+
+  return loading ? null : (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+          <label>How many transactions have you had this year</label>
+          <br></br>
+          <select value={option} onChange={onChange}>
+            {options.map((x) => (
+              <option key={x}>{x}</option>
+            ))}
+          </select>
+          <input type="submit" value="Submit" />
+        </form>
       </header>
     </div>
   );
